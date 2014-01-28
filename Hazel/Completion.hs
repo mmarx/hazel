ccaf416b7c4952f8e22d8599863bbee5f34571ca
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2014-01-28 19:55:48 +0100

    Use HashMaps during the completion procedure
    Instead of repeatedly replacing functions by functions that differ only
    in a single point, use HashMaps to hold the mapping. This makes lookup
    complexity logarithmic in the number of replacements, and thus improves
    over the linear complexity of the previous approach, yielding
    a significant speedup.

--- Hazel/Completion.hs
+++ Hazel/Completion.hs
@@ -12,8 +12,18 @@ module Hazel.Completion
        where
 
 import Control.Monad.State.Lazy
-import Data.List
+import Data.List ( intersect
+                 , nub
+                 )
 import Data.Set (elems)
+import Data.Hashable (Hashable)
+import Data.HashMap.Strict ( HashMap
+                           , empty
+                           , fromList
+                           , lookupDefault
+                           , singleton
+                           , union
+                           )
 import Hazel.Core
 
 
@@ -23,6 +33,8 @@ import Hazel.Core
 data CGraph = CGraph { getNodes :: Concept -> [Node]
                      , getRoles :: Role -> [CEdge]
                      }
+
+data CGraph' = CGraph' (HashMap Concept [Node]) (HashMap Role [CEdge])
 
 -- data structure for factorized neighborhood of concepts
 -- according to the PhD thesis of Dr. Suntisrivaraporn (on page 69)
@@ -44,79 +56,74 @@ type CEdge = (Concept, Concept)
 
 data CState = CState [Node] [CEdge]
               deriving (Eq)
-type Completion = State CState CGraph
+type Completion = State CState CGraph'
 
 
 -- Auxiliary functions
 
--- | Returns a function that is like f, except one argument gets a new value
-except :: (Eq a)
-          => (a -> b) -- ^ original function
-          -> a        -- ^ argument whose function value should be replaced
-          -> b        -- ^ new value
-          -> a -> b
-except f x' y' x
-    | x == x'   = y'
-    | otherwise = f x
+except :: (Eq k, Hashable k) => HashMap k v -> k -> v -> HashMap k v
+except m x' y' = union (singleton x' y') m
 
-initGraph :: [Node] -> CGraph
+initGraph :: [Node] -> CGraph'
 -- ^ Initialization of the completion graph
 initGraph allNodes =
-    CGraph n r
+    CGraph' n empty
   where
-    n Top         = nub $ Top : allNodes
-    n x@(Name _)  = [x]
-    n x@(Dummy _) = [x]
-    n _           = error "Complex concepts are not valid nodes in CGraph"
-    r _           = []
-
+    n = fromList [(Top, nub $ Top : allNodes)]
 
 -- Functions applying Completion Rules
 
-cr1 :: GCI -> CGraph -> Concept -> Completion
-cr2 :: GCI -> CGraph -> Concept -> Completion
-cr3 :: GCI -> CGraph -> Concept -> Completion
-cr4 :: GCI -> CGraph -> (Concept, Concept) -> Completion
+cr1 :: GCI -> CGraph' -> Concept -> Completion
+cr2 :: GCI -> CGraph' -> Concept -> Completion
+cr3 :: GCI -> CGraph' -> Concept -> Completion
+cr4 :: GCI -> CGraph' -> (Concept, Concept) -> Completion
 
 changeNode :: Node -> State CState ()
 changeNode node = do
     CState ns es <- get
     put $ CState (node:ns) es
 
-cr1 (Subclass c' d) (CGraph n r) c
+cr1 (Subclass c' d) (CGraph' nm rm) c
     | (c `notElem` n d) && (c `elem` n c') = do
         changeNode c
-        return $ CGraph n' r
-    | otherwise = return $ CGraph n r
+        return $ CGraph' n' rm
+    | otherwise = return $ CGraph' nm rm
   where
-    n' = except n d (c:n d)
+    n x = lookupDefault [x] x nm
+    n' = except nm d (c:n d)
 
-cr2 (Subclass (And c1 c2) d) (CGraph n r) c
+cr2 (Subclass (And c1 c2) d) (CGraph' nm rm) c
     | (c `elem` n c1) && (c `elem` n c2) && (c `notElem` n d) = do
         changeNode c
-        return $ CGraph n' r
-    | otherwise = return $ CGraph n r
+        return $ CGraph' n' rm
+    | otherwise = return $ CGraph' nm rm
   where
-    n' = except n d (c:n d)
+    n x = lookupDefault [x] x nm
+    n' = except nm d (c:n d)
+
 cr2 _ _ _ = error "Application of Rule CR2 not possible"
 
-cr3 (Subclass c' (Exists role d)) (CGraph n r) c
+cr3 (Subclass c' (Exists role d)) (CGraph' nm rm) c
     | (c `elem` n c') && ((c, d) `notElem` r role) = do
         CState ns es <- get
         put $ CState ns ((c, d):es)
-        return $ CGraph n r'
-    | otherwise = return $ CGraph n r
+        return $ CGraph' nm r'
+    | otherwise = return $ CGraph' nm rm
   where
-    r' = except r role ((c, d):r role)
+    n x = lookupDefault [x] x nm
+    r x = lookupDefault [] x rm
+    r' = except rm role ((c, d):r role)
 cr3 _ _ _ = error "Application of Rule CR3 not possible"
 
-cr4 (Subclass (Exists role d') e) (CGraph n r) (c, d)
+cr4 (Subclass (Exists role d') e) (CGraph' nm rm) (c, d)
     | ((c, d) `elem` r role) && (d `elem` n d') && (c `notElem` n e) = do
         changeNode c
-        return $ CGraph n' r
-    | otherwise = return $ CGraph n r
+        return $ CGraph' n' rm
+    | otherwise = return $ CGraph' nm rm
   where
-    n' = except n e (c:n e)
+    n x = lookupDefault [x] x nm
+    r x = lookupDefault [] x rm
+    n' = except nm e (c:n e)
 cr4 _ _ _ = error "Application of Rule CR4 not possible"
 
 
@@ -127,21 +134,25 @@ emptyState = CState [] []
 
 complete :: TBox -> CGraph
 complete (TBox gcis cs _) =
-    go . initGraph $ elems cs
+  toCGraph . go . initGraph $ elems cs
   where
-    go :: CGraph -> CGraph
+    go :: CGraph' -> CGraph'
     go graph
         | state' == emptyState = graph'
         | otherwise            = go graph'
       where
         (graph', state') = runState (iterateGCI graph gcis) emptyState
+    toCGraph (CGraph' nm rm) = CGraph n r
+      where n x = lookupDefault [x] x nm
+            r x = lookupDefault [] x rm
 
-iterateNodes :: CGraph -> GCI -> Completion
-iterateNodes cG@(CGraph n r) gci = case gci of
-    (Subclass (And c d) _)       -> foldM (cr2 gci) cG (n c `intersect` n d)
-    (Subclass c' (Exists _ _))   -> foldM (cr3 gci) cG (n c')
-    (Subclass (Exists role _) _) -> foldM (cr4 gci) cG (r role)
-    (Subclass c' _)              -> foldM (cr1 gci) cG (n c')
+iterateNodes :: CGraph' -> GCI -> Completion
+iterateNodes cG@(CGraph' n r) gci = case gci of
+    (Subclass (And c d) _)       -> foldM (cr2 gci) cG (n' c `intersect` n' d)
+    (Subclass c' (Exists _ _))   -> foldM (cr3 gci) cG (n' c')
+    (Subclass (Exists role _) _) -> foldM (cr4 gci) cG (lookupDefault [] role r)
+    (Subclass c' _)              -> foldM (cr1 gci) cG (n' c')
+    where n' c = lookupDefault [c] c n
 
-iterateGCI :: CGraph -> [GCI] -> Completion
+iterateGCI :: CGraph' -> [GCI] -> Completion
 iterateGCI = foldM iterateNodes
